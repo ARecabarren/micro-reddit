3d96be019bb84f09cfc95f08fcb6214a8e079743
Arecabarren <arecabarren14@alumnos.utalca.cl>
2023-11-08 11:09:52 -0300

    Add validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+
+    #Validations
+    validates :username, presence: true, uniqueness: true, length: { in: 4..14 }
+    validates :email, presence: true, uniqueness: true
+    validates :password, presence: true, length: { in: 6..20 }
 end
